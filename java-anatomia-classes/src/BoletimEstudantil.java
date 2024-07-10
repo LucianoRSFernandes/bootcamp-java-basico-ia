4996c7a69971683c46575b9673f06df806146ddc
lucianorsfernandes <luciano_r_s_fernandes@yahoo.com.br>
2024-07-10 20:07:11 -0300

    Anatomia classes BoletimEstudantil

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,11 @@
+public class BoletimEstudantil {
+    public static void main (String[] args) {
+        int mediaFinal =2;
+            if (mediaFinal <6)
+                System.out.println ("Reparovado");
+            else if (mediaFinal ==6)
+                System.out.println ("Prova Minerva");
+            else
+                System.out.println ("Aprovado");
+    }
+}
